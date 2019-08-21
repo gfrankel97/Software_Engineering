2af3e3f7f9c90a1b3aa5eeca008f317a682ff83a
gfrankel97 <gabriel.frankel@louisville.edu>
2019-08-20 21:13:00 -0400

    initial commit - using Docker, Python, Angular stack

---
+++ infra/resource-group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "rgSoftwareEngineering" {
+    name     = "rgSoftwareEngineering"
+    location = "East US"
+}
---
+++ source/backend/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, render_template, send_from_directory
+from flask_cors import CORS, cross_origin
+
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+#serve static files
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+    return send_from_directory(os.path.join(root_dir, 'backend', 'templates', 'html'), path)
+
+
+@app.route('/')
+def app_entrypoint():
+    return render_template('app.jinja2')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
